8e08760dd448d36f9243a157fb9e344ac25b94d3
coreosg <coreosgamer@gmail.com>
2023-04-14 10:50:34 -0300

    feat: Add CPF mask fixing and menu options
    - Implement a menu with options to save repeated CPFs, fix CPF mask, or exit
    - Add a function to fix the CPF mask for the entire DataFrame
    - Update the script to allow users to input the file path or just the file name

--- main.py
+++ main.py
@@ -7,48 +7,74 @@ def fix_cpf_mask(cpf):
     return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"  # Format CPF
 
 def openCSV(file):
-    if(not os.path.exists(file)):
-        print('Arquivo não encontrado! certifique-se que o arquivo está na mesma pasta do programa')
+    if not os.path.exists(file):
+        print('Arquivo não encontrado! certifique-se que o arquivo está na pasta correta')
         sleep(5)
         exit()
     return pd.read_csv(file, sep=',', encoding='utf-8')
 
-def repeatedCPF(df):
+def findCPFColumn(df):
     # Find the CPF column header
     cpf_column = None
     for col in df.columns:
-        if 'CPF' in col:
+        if 'cpf' in col.lower():  # Perform a case-insensitive search
             cpf_column = col
-            break
+            return cpf_column
+    print("coluna com CPF não encontrada")
+    return None
 
-    if cpf_column is None:
-        print("coluna CPF não encontrada")
-        return pd.DataFrame()
-
+def fixCPFmask(df):
+    cpf_column = findCPFColumn(df)
     # Fix the CPF mask for the CPF column
     df[cpf_column] = df[cpf_column].apply(fix_cpf_mask)
-    
-    # Find rows with repeated CPF values
-    repeated_cpf = df[df.duplicated(subset=cpf_column, keep=False)]
-    return repeated_cpf
+    return df
 
-
-
-def saveExcel(df, file):
+def saveExcel(df, file, suffix):
+    file = file.removesuffix('.csv') + suffix
     if os.path.exists(file + '.xlsx'):
-        os.remove(file + '.xlsx')  # Add the correct file extension here
+        os.remove(file + '.xlsx')
         print('Arquivo já existente, removendo arquivo antigo...')
     df.to_excel(file + '.xlsx', index=False)
     print('Arquivo salvo com sucesso! \n' + file + '.xlsx')
 
+def repeatedCPF(df):
+    # Fix the CPF mask for the CPF column
+    df = fixCPFmask(df)
+    cpf_column = findCPFColumn(df)
+    # Find rows with repeated CPF values
+    repeated_cpf = df[df.duplicated(subset=cpf_column, keep=False)]
+    return repeated_cpf
 
+def menu():
+    print('1- Salvar os CPF repetidos')
+    print('2- Consertar máscara do CPF')
+    print('3- Sair')
+    opt = int(input('Digite a opção desejada: '))
+    return opt
+
+def getFileName():
+    print('Digite o caminho completo do arquivo ou apenas o nome do arquivo (se estiver na mesma pasta): ')
+    nomeArquivo = input()
+    return nomeArquivo
+
+def main():
+    opt = menu()
+    if opt == 1:
+        nomeArquivo = getFileName()
+        df = openCSV(nomeArquivo)
+        repeated_cpf = repeatedCPF(df)
+        saveExcel(repeated_cpf, nomeArquivo, '_CPF_repetidos')
+    elif opt == 2:
+        nomeArquivo = getFileName()
+        df = openCSV(nomeArquivo)
+        df = fixCPFmask(df)
+        saveExcel(df,  nomeArquivo, '_CPF_corrigido')
+    else:
+        print('Falow!')
+        sleep(1)
+        return False
+    return True
 
 if __name__ == '__main__':
-    nomeArquivo = input('Digite o nome do arquivo: ')
-    df = openCSV(nomeArquivo)
-    repeated_cpf = repeatedCPF(df)
-    saveExcel(repeated_cpf,  nomeArquivo.removesuffix('.csv') + '_cpf_repetidos')
-    sleep(5)
-    
-
-    
+    while main():
+        pass
